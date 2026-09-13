f13061989430f24102a9e8bdb93d538681a03595
saugat1070 <saugatgiri1070@gmail.com>
2026-09-13 23:20:34 +0545

    refactor: use custom ServeMux and update health endpoint to return JSON response

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,9 +8,20 @@ import (
 
 func main() {
 
-	http.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("okay")) // conversion from string to byte
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK) // go provide http status code as constant
+		w.Header().Set("content-type", "application/json")
+		w.Write([]byte(`{"status": "okay"}`)) // conversion from string to byte
 	})
+
+	// http.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.WriteHeader(http.StatusOK) // go provide http status code as constant
+	// 	w.Header().Set("content-type", "application/json")
+	// 	w.Write([]byte(`{"status": "okay"}`)) // conversion from string to byte
+	// })
+	// @description: adding default http DefaultServeMux can cause script injection on our server
+	// so we use mux from http package instead of default ServeMux
 
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
